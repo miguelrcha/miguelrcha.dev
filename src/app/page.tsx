3ff75e7288ceda6e42dada2fe51aeb26ca94e951
Miguel Rocha <miguellrochaxavier@gmail.com>
2026-06-24 13:39:02 -0300

    docs: remove eternosDevs experience on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,19 +72,7 @@ const WORK_EXPERIENCE = [
       "With a strong focus on System Development, I design and build scalable applications using Power Platform, developing advanced dashboards and automated workflows to ensure productivity, reduce rework, and optimize user experience.",
     ],
   },
-  {
-    company: "EternosDevs",
-    companyUrl: "",
-    companyDescription: "Minecraft server focused on survival multiplayer [SMP] and engaging experience for players of all ages, with a strong emphasis on community and creativity.",
-    role: "Co-founder & Backend Engineer",
-    period: "Apr 2026 - Present",
-    tags: ["Remote", "Java", "MySQL" ],
-    bullets: [
-      "Co-founded the project with two partners and leading the technical development, including programming, system architecture, and design decisions.",
-      "Currently building a Minecraft SMP server (pre-launch), developing backend systems in Java and continuously improving my backend development skills."
-    ],
-  }
-
+  
 ];
 
 const FORMATION = [
